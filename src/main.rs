1c6331fac6429152f7788abe9956026a3879786e
Ebenezer Don <ebenezerdonu@gmail.com>
2020-07-20 23:29:52 +0100

    render home handlebar template for index route

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,23 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket_contrib::templates::Template;
+use serde::Serialize;
+
 #[get("/")]
-fn index() -> &'static str {
-  "Landing Page"
+fn index() -> Template {
+  #[derive(Serialize, Debug)]
+  struct Context {
+    first_name: String,
+    last_name: String
+  }
+
+  let context = Context {
+    first_name: String::from("Ebenezer"),
+    last_name: String::from("Don")
+  };
+
+  Template::render("index", context)
 }
 
 fn main() {
